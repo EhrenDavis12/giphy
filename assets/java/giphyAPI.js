442cde071367a6085fe3d0b178423ad925f9f578
Ehren Davis <ehren.davis12@gmail.com>
2018-09-10 21:23:26 -0700

    monday work

--- assets/java/giphyAPI.js
+++ assets/java/giphyAPI.js
@@ -1,46 +1,18 @@
 
-
 var giphyAPI = {
     getGiphyAPI: function (searchWord, callBackFunction) {
-        searchWord = giphyAPI.cleanSearchString(searchWord) || "facePalm";
+        searchWord = baseAPI.cleanSearchString(searchWord) || "facePalm";
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             searchWord + "&api_key=w2TbIZFx8ZPKjMUHPf28Hu8R1kSt1DZN" +
             "&limit=10";
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                let results = response.data;
-                console.debug("getGiphyAPI and results:");
-                console.debug(results);
-                localStorage.setItem("lastCall", JSON.stringify(results));
-                callBackFunction(results);
-            });
-
+        baseAPI.getBaseAPI(queryURL, callBackFunction);
     },
 
     getLocalSample: function (searchWord, callBackFunction) {
-        let sampleAPI = JSON.parse(localStorage.getItem("lastCall"));
-
-        if (sampleAPI === null){
-            giphyAPI.getGiphyAPI(searchWord, callBackFunction);
-        }
-        else{
-            console.debug("getLocalSample and results:");
-            console.debug(sampleAPI);
-            callBackFunction(sampleAPI)
-        }
+        baseAPI.getLocalBaseSample(searchWord, giphyAPI.getGiphyAPI, callBackFunction);
     },
 
-    cleanSearchString: function (str) {
-        try {
-            let returnWord = str.trim();
-            returnWord = returnWord.replace(/ /g, "");
-            return returnWord;
-        } catch{
-            return null;
-        }
+    getDataFromResponse: function(response){
+        return  response.data
     }
 }
